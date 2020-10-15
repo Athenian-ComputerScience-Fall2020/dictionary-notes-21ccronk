035445a8e7fbc3ce3c27489d755c67726ace9b63
21ccronk <21ccronk@athenian.org>
2020-10-15 16:31:17 +0000

    Dictionary notes

--- my_code.py
+++ my_code.py
@@ -1,2 +1,19 @@
 # Use this to take notes on the Edpuzzle video. Try each example rather than just watching it - you will get much more out of it!
 #  
+
+student = {'name': 'john','age': 25, 4:['jellyfish','sea otter']}
+print(student['name'])
+print(student[4])
+
+student.update({'name':'sara'})
+
+print(student['name'])
+
+#del student['age']
+#print(student)
+
+#age = student.pop('age')
+#print(25)
+
+for key, value in student.items():
+    print(key, value)
